95e6305c3648f8975a2ca9990fa4c227d3f572a2
toshisugar <39898922+toshisugar@users.noreply.github.com>
2018-10-30 15:27:18 +0900

    UIつくった

---
+++ ZipFileApplication/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZipFileApplication
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string zipPass = System.Web.Security.Membership.GeneratePassword(8, 0);
+        }
+
+        private void ZipPass(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+    }
+}
