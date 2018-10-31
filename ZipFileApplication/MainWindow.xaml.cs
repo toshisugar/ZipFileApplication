491e0169b807b8ab16a0fdafba75ccc73729f93e
toshisugar <39898922+toshisugar@users.noreply.github.com>
2018-10-31 11:43:25 +0900

    Zipファイル作成まで実装

--- ZipFileApplication/MainWindow.xaml.cs
+++ ZipFileApplication/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Forms = System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+
 
 namespace ZipFileApplication
 {
@@ -23,17 +16,60 @@ namespace ZipFileApplication
         public MainWindow()
         {
             InitializeComponent();
-
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DirectoryPath.Text = Path.Combine(desktopPath, "ZipFileApplication");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //Zipファイルの作成を行う
+        private void ZipButton_Click(object sender, RoutedEventArgs e)
         {
+            //パスワードを自動生成し、そのパスワードが設定されたZipファイルを作成する
             string zipPass = System.Web.Security.Membership.GeneratePassword(8, 0);
+
+            //圧縮するファイルが入っているフォルダのパス
+            string sourceDirectory = DirectoryPath.Text;
+            //サブディレクトリも圧縮するかどうか
+            bool recurse = true;
+
+            //作成するZipファイルの名前
+            //ファイルが既に存在している場合は、上書きされる
+            string zipFileDirectoryAndName = Path.Combine(sourceDirectory, Path.ChangeExtension(SetZipFileName.Text, ".zip"));
+
+            FastZip fastZip = new FastZip();
+            //trueなら空のフォルダもZipファイルに入れる。(デフォルトはfalse)
+            fastZip.CreateEmptyDirectories = false;
+            //ZIP64を使うか。デフォルトはDynamicで、状況に応じてZIP64を使う。
+            //（大きなファイルはZIP64でしか圧縮できないが、対応していないアーカイバもある）
+            fastZip.UseZip64 = UseZip64.Dynamic;
+            //パスワードを設定
+            fastZip.Password = zipPass;
+            //ZIPファイルを作成
+            fastZip.CreateZip(zipFileDirectoryAndName, sourceDirectory, recurse, null, null);
+
         }
 
         private void ZipPass(object sender, TextCompositionEventArgs e)
         {
 
         }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // フォルダー参照ダイアログのインスタンスを生成
+            var dlg = new Forms.FolderBrowserDialog();
+
+            // ボックスの説明文
+            dlg.Description = "Zipにしたいファイル・フォルダが入っているフォルダーを選択してくれよな！";
+
+            // ダイアログボックスを表示
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // 選択されたフォルダーパスをフォルダパスのテキストボックスに入力
+                DirectoryPath.Text = dlg.SelectedPath;
+            }
+        }
+
+
     }
 }
+
